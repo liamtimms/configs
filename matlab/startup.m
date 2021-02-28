6338067730cc045e357a3800b1cf398b836ad7d0
Liam Timms <timms5000@gmail.com>
2021-02-28 16:30:36 -0500

    adding matlab startup config

---
+++ matlab/startup.m
@@ -0,0 +1,25 @@
+%------------ FreeSurfer -----------------------------%
+fshome = getenv('FREESURFER_HOME');
+fsmatlab = sprintf('%s/matlab',fshome);
+if (exist(fsmatlab) == 7)
+    addpath(genpath(fsmatlab));
+end
+clear fshome fsmatlab;
+%-----------------------------------------------------%
+
+%------------ FreeSurfer FAST ------------------------%
+fsfasthome = getenv('FSFAST_HOME');
+fsfasttoolbox = sprintf('%s/toolbox',fsfasthome);
+if (exist(fsfasttoolbox) == 7)
+    path(path,fsfasttoolbox);
+end
+clear fsfasthome fsfasttoolbox;
+%-----------------------------------------------------%
+
+%------------ SPM 12 ------------------------%
+spmhome = '/opt/spm12';
+if (exist(spmhome) == 7)
+    path(path,spmhome);
+end
+clear spmhome;
+%-----------------------------------------------------%
