99c3310091356b7c85b4296c41b1eadecdf79c9f
Liam Timms <timms5000@gmail.com>
2022-08-27 01:03:00 -0400

    moving configuration over to lua slowly

---
+++ nvim/basic_settings.lua
@@ -0,0 +1,85 @@
+-- Options
+local autocmd = vim.api.nvim_create_autocmd
+local opt = vim.opt
+
+-- show line number highlight current line
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+
+-- splits window more naturally
+opt.splitright = true
+opt.splitbelow = true
+
+-- mouse support
+opt.mouse = "a"
+
+-- tabs into spaces
+opt.expandtab = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+
+-- leaving out latex bit from old config for now
+
+-- KEYMAPS -------------
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal mode
+-- Shortcutting split navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+-- keymap("n", "<S-l>", ":bnext<CR>", opts)
+-- keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Navigate buffers with bufferline
+keymap("n", "<S-l>", ":BufferLineCycleNext<CR>", opts)
+keymap("n", "<S-h>", ":BufferLineCyclePrev<CR>", opts)
+
+-- Move text up and down
+-- keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+-- keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- jump list fix, maybe not deeded anymore
+keymap("n", "<C-i>", "<C-i>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- -- Move text up and down
+-- keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+-- keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+-- keymap("v", "p", '"_dP', opts)
+
+P = function(v)
+	print(vim.inspect(v))
+	return v
+end
---
+++ nvim/plug_lua_settings.lua
@@ -0,0 +1,120 @@
+-- treesitter
+require("nvim-treesitter.configs").setup({
+	ensure_installed = {
+		"bash",
+		"python",
+		"c",
+		"lua",
+		"rust",
+		"javascript",
+		"bibtex",
+		"cmake",
+		"css",
+		"dockerfile",
+		"html",
+		"http",
+		"json",
+		"json5",
+		"latex",
+		"markdown",
+		"regex",
+		"scss",
+		"vim",
+		"yaml",
+	}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "markdown" }, -- list of language that will be disabled
+	},
+})
+-- lualine
+require("lualine").setup({
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		-- component_separators = { left = '', right = ''},
+		-- section_separators = { left = '', right = ''},
+		disabled_filetypes = {},
+		always_divide_middle = true,
+		globalstatus = true,
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = {
+			"filename",
+			-- {
+			-- 	"buffers",
+			-- 	buffers_color = {
+			-- 		-- Same values as the general color option can be used here.
+			-- 		active = "lualine_{section}_normal", -- Color for active buffer.
+			-- 		inactive = "lualine_{section}_inactive", -- Color for inactive buffer.
+			-- 	},
+			-- },
+		},
+		lualine_x = { "g:coc_status", "filetype" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {},
+})
+
+-- bufferline without font issues
+require("bufferline").setup({
+	highlights = {
+		buffer_selected = {
+			bold = true,
+			italic = false,
+		},
+	},
+})
+
+-- git signs
+require("gitsigns").setup()
+
+--comments
+require("Comment").setup()
+
+--fzf
+-- require('fzf-lua').setup()
+
+-- vim.opt.list = true
+-- vim.opt.listchars:append("space:⋅")
+-- vim.opt.listchars:append("eol:↴")
+
+require("indent_blankline").setup({
+	space_char_blankline = " ",
+	show_current_context = true,
+	show_current_context_start = true,
+})
+
+-- explorer
+require("nvim-tree").setup()
+
+-- alpha
+-- require('alpha').setup(require'alpha.themes.startify'.config)
+
+vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>lua require('fzf-lua').files()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>fb",
+	"<cmd>lua require('fzf-lua').buffers()<CR>",
+	{ noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>fg",
+	"<cmd>lua require('fzf-lua').live_grep()<CR>",
+	{ noremap = true, silent = true }
+)
+
+require("prosemode").setup()
